b08f390ab7093fc27ecd0b1d43fa8f88c5ba5ac3
Fabricio S Antunes <fabricio.antunes@bludata.com.br>
2021-06-29 21:39:48 -0300

    alterando apollo client

--- src/data/apolloClient.js
+++ src/data/apolloClient.js
@@ -1,8 +1,24 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
-export const client = new ApolloClient({
+const enchancedFetch = (url, init) => {
+    return fetch(url, {
+        ...init,
+        headers: {
+            ...init.headers,
+            'Access-Control-Allow-Origin': '*',
+        },
+    }).then(response => response)
+}
+
+const httpLink = new HttpLink({
+    uri: "http://testefront.dev.softplan.com.br/graphql",
     fetchOptions: {
         mode: 'no-cors',
     },
+    fetch: enchancedFetch,
+})
+
+export const client = new ApolloClient({
+    link: httpLink,
     cache: new InMemoryCache({
         typePolicies: {
             Country: {
@@ -36,5 +52,4 @@ export const client = new ApolloClient({
             }
         }
     }),
-    uri: "http://testefront.dev.softplan.com.br/graphql"
 });
